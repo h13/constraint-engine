9115d36f803623dbb242ec2bfc7a260b4c414f8b
Hironori Takahashi <h13.webmail@gmail.com>
2026-03-02 07:29:46 +0900

    test: add Health resource 503 error path test
    Exercises the PDOException catch branch that returns status=error
    and db=disconnected when the database is unreachable.

--- tests/Resource/Page/HealthTest.php
+++ tests/Resource/Page/HealthTest.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace ConstraintEngine\App\Resource\Page;
 
+use Aura\Sql\ExtendedPdoInterface;
 use BEAR\Resource\ResourceObject;
 use ConstraintEngine\App\ResourceTestCase;
+use PDOException;
 
 use function assert;
 
@@ -18,5 +20,18 @@ class HealthTest extends ResourceTestCase
         $this->assertSame(200, $ro->code);
         $this->assertSame('ok', $ro->body['status']);
         $this->assertSame('connected', $ro->body['db']);
+    }
+
+    public function testOnGetReturns503WhenDatabaseFails(): void
+    {
+        $pdo = $this->createMock(ExtendedPdoInterface::class);
+        $pdo->method('fetchValue')->willThrowException(new PDOException('connection refused'));
+
+        $health = new Health($pdo);
+        $health->onGet();
+
+        $this->assertSame(503, $health->code);
+        $this->assertSame('error', $health->body['status']);
+        $this->assertSame('disconnected', $health->body['db']);
     }
 }
