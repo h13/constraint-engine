78c9016af252fd3cdd8d48396466b2dccee63bc2
Hironori Takahashi <h13.webmail@gmail.com>
2026-03-05 08:03:05 +0900

    fix: add --dbname to init-test-db.sh for correct psql connection
    Without --dbname, psql defaults to connecting to a database named
    after the user ('app'), which doesn't exist. Use POSTGRES_DB instead.

--- docker/db/init-test-db.sh
+++ docker/db/init-test-db.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE constraint_engine_test;
     GRANT ALL PRIVILEGES ON DATABASE constraint_engine_test TO $POSTGRES_USER;
 EOSQL
